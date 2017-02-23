7b0b312e78ad7e65db43b18364a90957185fd586
Lukas Ferreira <unlisislukasferreira@hotmail.com>
2017-02-23 18:53:09 -0300

    Adding PASSWORD_HASHERS on settings.py to optimize user authetication on tests.py

--- RESTAPI/agenda/agenda/settings.py
+++ RESTAPI/agenda/agenda/settings.py
@@ -103,6 +103,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# The default password hasher is rather slow by design. If you’re authenticating
+# many users in your tests, you may want to use a custom settings file and set
+# the PASSWORD_HASHERS setting to a faster hashing algorithm
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
