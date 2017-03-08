366ec533257cf36244790bd9317e390fec2cba8f
Lukas Ferreira <unlisislukasferreira@hotmail.com>
2017-03-08 19:00:06 -0300

    Minor changes

--- Agenda/src/components/main.js
+++ Agenda/src/components/main.js
@@ -14,15 +14,16 @@ class Main extends Component {
   }
 
   render() {
-    console.log(this.props.user);
-    if (this.props.user) {
+    const { user } = this.props;
+    console.log(user);
+    if (user) {
       return (
         <div>
           <h2>Ola</h2>
-          <h3>Username: {this.props.user.username}</h3>
-          <h3>Primeiro Nome: {this.props.user.first_name}</h3>
-          <h3>Sobrenome: {this.props.user.last_name }</h3>
-          <h3>Email: {this.props.user.email}</h3>
+          <h3>Username: {user.username}</h3>
+          <h3>Primeiro Nome: {user.first_name}</h3>
+          <h3>Sobrenome: {user.last_name }</h3>
+          <h3>Email: {user.email}</h3>
           <button className="btn btn-danger" onClick={this.logout.bind(this)} >Log Out</button>
         </div>
       );
