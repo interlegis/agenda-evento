840120828eb4ced176964b1f9087638fa83c6ae6
Lukas Ferreira <unlisislukasferreira@hotmail.com>
2017-06-08 18:48:04 -0300

    Atualizando cron.py: publicando eventos da agenda ja reservados e recebidos. Falta agora logica de enviar o email dos eventos da semana seguinte

--- RESTAPI/agenda/apps/rest_agenda/cron.py
+++ RESTAPI/agenda/apps/rest_agenda/cron.py
@@ -1,4 +1,23 @@
-from .models import CronLog
+from rest_framework import status
+from rest_framework.response import Response
+from .models import CronLog, Reserva
 
 def my_scheduled_job():
-    CronLog.objects.create()
+    """
+    Update all instances of the queryset. Then send email to all users and
+    create a Log that has the timestamp of the send
+    """
+    try:
+        reservas = Reserva.objects.select_related().filter(status=u'R',
+                                                           recebido=True,
+                                                           evento__publicado_agenda=False)
+
+        for i in range(len(reservas)):
+            reservas[i].evento.publicado_agenda = True
+            reservas[i].evento.save()
+            reservas[i].save()
+
+        CronLog.objects.create()
+        return Response(status=status.HTTP_200_OK)
+    except:
+        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
