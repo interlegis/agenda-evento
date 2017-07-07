b9e19a89a5cdf28577255684bf80e06b5a0697c1
Matheus Veleci <matheusveleci@aluno.unb.br>
2017-07-07 11:33:58 -0300

    Ajusta Dockerfie na parte da Agenda

--- Agenda/webpack.production.config.js
+++ Agenda/webpack.production.config.js
@@ -1,4 +1,4 @@
-
+const webpack = require('webpack');
 module.exports = {
   entry: [
     './src/index.js'
@@ -8,6 +8,11 @@ module.exports = {
     publicPath: '/',
     filename: 'main.js'
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    })
+  ],
   module: {
     loaders: [
       {
