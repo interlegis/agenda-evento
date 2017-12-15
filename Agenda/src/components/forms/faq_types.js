2a5a476e57fb4e0654541fd84924b0a4480eb807
Lukas Ferreira <unlisislukasferreira@hotmail.com>
2017-12-15 18:00:07 -0200

    refatorando faq.js para render dinamico

---
+++ Agenda/src/components/forms/faq_types.js
@@ -0,0 +1,81 @@
+export const FIELD_FAQ = {
+  first: {
+    id:'collapseOne',
+    parentid:'accordion-1',
+    titulo:'About Costing',
+    items: {
+      item1: 'No capital expense',
+      item2: 'No operations cost',
+      item3: 'Up to 60% cost savings when compared to current offshore costs'
+    }
+  },
+  second: {
+    id:'collapseTwo',
+    parentid:'accordion-1',
+    titulo:'How we operate?',
+    items: {
+      item1: 'Robust process.',
+      item2: 'Flexible model',
+      item3: 'Transparent operations'
+    }
+  },
+  third: {
+    id:'collapseThree',
+    parentid:'accordion-1',
+    titulo:'Team work',
+    items: {
+      item1: 'Work single project at a time',
+      item2: 'Availability of resources with skills hard to find locally',
+      item3: 'Low employee attrition.',
+      item4: 'Employees with previous experience in working directly with international (UK, US &amp; EU) clients',
+      item5: 'Complete management control over your dedicated resources.',
+      item6: 'Our engagement model allows you to treat and manage your resources as your own employees'
+    }
+  },
+  fourth: {
+    id:'collapseFour',
+    parentid:'accordion-1',
+    titulo:'Quality Assurance',
+    items: {
+      item1: 'As client has direct control over output or result of candidate, quality will be maintained as per standards at client end.',
+      item2: 'As quality people in terms of experience and skills are involved quality can be better controlled as per requirements'
+    }
+  }
+};
+
+export const FIELD_FAQ2 = {
+  first: {
+    id:'one',
+    parentid:'accordion-2',
+    titulo:'Why I need to',
+    items: {
+      item1: 'Cost benefits.',
+      item2: 'You can concentrate more on to increase customer base or other core business process.'
+    }
+  },
+  second: {
+    id:'Two',
+    parentid:'accordion-2',
+    titulo:'How much needed',
+    items: {
+      item1: 'You can hire candidate for minimum of 4 months. But pilot program can be run for 1 month.'
+    }
+  },
+  third: {
+    id:'Three',
+    parentid:'accordion-2',
+    titulo:'Is our service cost effective and qualified?',
+    items: {
+      item1: '1. Yes! Our service is about 40% less costly compared to existing outsourcing service providers. You can hire 3 years experience person in general skills at an average cost of USD 1800$ per month.'
+    }
+  },
+  fourth: {
+    id:'Four',
+    parentid:'accordion-2',
+    titulo:'Is there project manager for each project?',
+    items: {
+      item1: 'For each outsourcing project where more than 3 resources are working, we provide dedicated project manager at free of cost. He will be in contact with client as well as have responsibility of managing entire project.',
+      item2: 'This project manager cost will be bearded by Nimetler Technologies.In all other cases, Nimetler in house project manager will.'
+    }
+  }
+}
