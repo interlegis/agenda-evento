3207d930a5deac34689fd7393f0f30b09bac348b
Lukas Ferreira <unlisislukasferreira@hotmail.com>
2018-02-16 10:52:17 -0200

    Adding new changes to populate_models() into signals.py

--- RESTAPI/agenda/apps/authentication/signals.py
+++ RESTAPI/agenda/apps/authentication/signals.py
@@ -35,13 +35,16 @@ def populate_models(sender, instance=None, **kwargs):
             if usuario == 'admin':
                 novo_usuario = User.objects.create_superuser(username=usuario,
                                                              email='',
+                                                             password=ADMIN_PASSWORD,
                                                              first_name='Administrador')
-                novo_usuario.set_password(ADMIN_PASSWORD)
+                #novo_usuario.set_password(ADMIN_PASSWORD)
                 novo_usuario.groups.add(Group.objects.get(name=groups[0]))
             else:
                 novo_usuario = User.objects.create(username=usuario,
+                                                   email='',
+                                                   password=SECRETARY_PASSWORD, 
                                                    first_name='Primeira Secretaria')
-                novo_usuario.set_password(SECRETARY_PASSWORD)
+                #novo_usuario.set_password(SECRETARY_PASSWORD)
                 if usuario == 'operador_primeira_secretaria':
                     novo_usuario.groups.add(Group.objects.get(name=groups[1]))
 
